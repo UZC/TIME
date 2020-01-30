ba1a71309ab109b4f885988fbc24e7e63ad284c7
azlok21 <56533909+azlok21@users.noreply.github.com>
2020-01-31 01:44:00 +0200

    Added Laser skill and cooldown

---
+++ TimeManipulation/Assets/Scripts/Laser.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    LineRenderer _lineRend;
+    [SerializeField]
+    public GameObject player;
+    [SerializeField]
+    public float cooldown;
+    [SerializeField]
+    public float duration;
+    private float nextActivity;
+    private float currentTime = 0;
+    [SerializeField]
+    SkillCooldown sc;
+    [SerializeField]
+    MeleeAttack enemyAttack;
+
+
+   
+
+    //private void Update()
+    //{
+
+    //    _lineRend.SetPosition(0, new Vector2(10000, 10000));
+    //    _lineRend.SetPosition(1, new Vector2(10000, 10000));
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        Ray2D ray = new Ray2D(transform.position, transform.up);
+    //        RaycastHit2D hit2D = Physics2D.Raycast(player.transform.position + new Vector3(2f, 2f), ray.direction, 50f);
+    //        _lineRend.SetPosition(0, player.transform.position);
+    //        if (hit2D.collider)
+    //        {
+    //            Debug.Log(hit2D.collider.name);
+    //            hit2D.transform.localScale = new Vector3(3.0f, 3.0f, 0.0f);
+    //            enemyAttack.damage
+    //        }
+    //        Debug.Log(hit2D.point);
+    //        _lineRend.SetPosition(1, new Vector2(player.transform.position.x, player.transform.position.y) + ray.direction * 50f);
+    //    }
+    //}
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            LaserStrike(new Vector3(2f, 2f));
+    }
+    public void LaserStrike(Vector3 Scale)
+    {
+       
+        _lineRend = GetComponent<LineRenderer>();
+        _lineRend.SetPosition(0, new Vector2(10000, 10000));
+        _lineRend.SetPosition(1, new Vector2(10000, 10000));
+        if (Time.time > nextActivity)
+        {
+            
+                    Ray2D ray = new Ray2D(transform.position, transform.up);
+                    RaycastHit2D hit2D = Physics2D.Raycast(player.transform.position + new Vector3(0f, 2f), ray.direction, 50f);
+                    _lineRend.SetPosition(0, player.transform.position);
+                    if (hit2D.collider)
+                    {
+                        hit2D.transform.localScale = Scale;
+                        enemyAttack.damage = 1;
+                    }
+                    _lineRend.SetPosition(1, new Vector2(player.transform.position.x, player.transform.position.y) + ray.direction * 50f);
+                    currentTime = currentTime + Time.deltaTime;
+                    Debug.Log(currentTime);
+                
+                
+                nextActivity = Time.time + cooldown;
+                sc.StartCooldown(cooldown);
+
+
+        }
+    }
+    IEnumerator Wait1second()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
+
+
+
+
---
+++ TimeManipulation/Assets/Scripts/SkillCooldown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCooldown : MonoBehaviour
+{
+    float cooldown;
+    float currentCooldown = 0;
+    public Image cooldownIcon;
+    bool flagStart = false;
+    void Update()
+    {
+        if (flagStart)
+        {
+            if (currentCooldown >= cooldown)
+            {
+                cooldownIcon.fillAmount = 0;
+                flagStart = false;
+                currentCooldown = 0;
+            }
+            if (currentCooldown < cooldown)
+            {
+                currentCooldown += Time.deltaTime;
+                cooldownIcon.fillAmount = currentCooldown / cooldown;
+            }
+        }
+    }
+    public void StartCooldown(float cooldown)
+    {
+        this.cooldown = cooldown;
+        flagStart = true;
+    }
+}
